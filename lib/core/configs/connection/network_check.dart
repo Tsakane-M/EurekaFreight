af6fe7b7889e38ea1362af4c170078899cd2a0f8
Peter Mhlanga <petermh@tfg.co.za>
2024-02-14 21:36:48 +0200

    setup project

--- lib/core/configs/connection/network_check.dart
+++ lib/core/configs/connection/network_check.dart
@@ -21,12 +21,9 @@ class NChecking extends StatelessWidget {
       builder: (context, state) {
         if (state is ConnectedSucessState) {
           return const MainPage();
-          // return const MainScreen();
-        }
-        // else if (state is ConnectedFailureState) {
-        //   return const NoConnectionErorr();
-        // }
-        else {
+        } else if (state is ConnectedFailureState) {
+          return const NoConnectionErorr();
+        } else {
           // return Container();
           // TODO
           // return const NoConnectionErorr();
@@ -42,11 +39,11 @@ class NoConnectionErorr extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             // LoadingAnimationWidget.staggeredDotsWave(
             //     color: primaryColor, size: 50.0),
             // SizedBox(height: 3.h),
